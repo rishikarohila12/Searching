aec01bf8f7e640fb23e42a9252ef808b702d281c
rishikarohila12 <ruhelarishika@gmail.com>
2024-01-26 20:19:35 +0530

    changes

--- recursiveBinary.cpp
+++ recursiveBinary.cpp
@@ -1,36 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int binarySearch(int arr[],int lo,int hi, int target)
+int binarySearch(int arr[], int lo, int hi, int target)
 {
-    while(lo<=hi)
+    while (lo <= hi)
     {
-        int mid=lo+(hi-lo)/2;
-        if(arr[mid]==target)
+        int mid = lo + (hi - lo) / 2;
+        if (arr[mid] == target)
         {
             return mid;
         }
-        else if(arr[mid]<target)
+        else if (arr[mid] < target)
         {
-            return binarySearch(arr,mid+1,hi,target);
+            return binarySearch(arr, mid + 1, hi, target);
         }
-        else //if(arr[mid]>target)
+        else // if(arr[mid]>target)
         {
-            return binarySearch(arr,lo,mid-1,target);
+            return binarySearch(arr, lo, mid - 1, target);
         }
         return -1;
     }
 }
 int main()
 {
-    int arr[]={10,20,50,80,90,100};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    int target=50;
-    int result=binarySearch(arr,0,n-1,target);
-    if(result==-1)
+    int arr[] = {10, 20, 50, 80, 90, 100};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 50;
+    int result = binarySearch(arr, 0, n - 1, target);
+    if (result == -1)
     {
-        cout<<"Element not found in the array";
+        cout << "Element not found in the array";
     }
-    else{
-        cout<<"Element "<<arr[result] <<" has found ar index "<<result;
+    else
+    {
+        cout << "Element " << arr[result] << " has found ar index " << result;
     }
 }
